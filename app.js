30117b8d5548fbac850e85664a2df6443a9465cc
Manas <manas.codewritter@gmail.com>
2026-02-20 00:30:46 +0530

    some adjustments

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ main()
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/stayease');
+    await mongoose.connect(process.env.DB_URL);
 
 }
 
